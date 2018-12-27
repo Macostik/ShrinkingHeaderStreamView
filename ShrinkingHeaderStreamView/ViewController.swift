e77bdeb3aa5223c82decf69eb449b045d9ddb566
Macostik <y.granchenko@simcord.com>
2018-12-27 10:29:05 +0200

    Initial Commit

---
+++ ShrinkingHeaderStreamView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShrinkingHeaderStreamView
+//
+//  Created by Гранченко Юрий on 12/27/18.
+//  Copyright © 2018 Simcord. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
